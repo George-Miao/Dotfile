9d2902d8f6e238dae8e4c91450f8016004330da6
George-Miao <gm@miao.dev>
2021-09-21 18:43:10 +0800

    Add dunst

---
+++ .config/wal/templates/dunst.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+reload_dunst() {{
+   pkill dunst
+   dunst -frame_width 0 \
+       -lb "{color0}" \
+       -nb "{color0}" \
+       -cb "{color0}" \
+       -lf "{color7}" \
+       -bf "{color7}" \
+       -cf "{color7}" \
+       -nf "{color7}" &
+}}
+
+reload_dunst
